6c8230f3d118b001c4e83db0d22b867ba5c7428e
towin <cristian3211998@gmail.com>
2025-01-23 12:33:03 -0500

    Se crea autenticacion y se modifica diseño con tailwind

---
+++ src/resources/views/components/alert.blade.php
@@ -0,0 +1,21 @@
+@props(['id', 'color'])
+
+<div role="alert" id="{{ $id }}"
+    {!! $attributes->merge(['class' => "mb-4 opacity-95 relative flex w-full p-3 text-sm text-white bg-{$color}-500 rounded-md"]) !!}>
+    {{ $slot }}
+    <button
+        class="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5"
+        onclick="closeAlert('{{ $id }}')" type="button">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5"
+            stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+        </svg>
+    </button>
+</div>
+
+<script>
+    function closeAlert(id) {
+        const alertElement = document.getElementById(id);
+        alertElement.style.display = 'none';
+    }
+</script>
